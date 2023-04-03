dcb06d1f6812e046646e16c054e2400b28a3ba89
dg-data <66296000+dg-data@users.noreply.github.com>
2023-04-03 22:09:23 +0200

    Update main.js

--- main.js
+++ main.js
@@ -82,8 +82,9 @@ new Vue({ //export default {
   mounted: function () {
     // get yesterday's games
     var url =
-      'https://thingproxy.freeboard.io/fetch/'+'https://it.global.nba.com/stats2/scores/miniscoreboard.json'
+      'https://it.global.nba.com/stats2/scores/miniscoreboard.json'
     // + new Date(new Date().setFullYear(new Date().getFullYear() - 1)).toISOString().slice(0, 4) + '/scores/00_todays_scores.json'
+    axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*'
     axios.get(url, { crossdomain: true })
       .then(response => {
         var g = []
