c00b297d6f7c679b1d66b5f147296ac655b2d59a
dg-data <66296000+dg-data@users.noreply.github.com>
2023-04-03 21:10:38 +0200

    Update main.js

--- main.js
+++ main.js
@@ -82,13 +82,15 @@ new Vue({ //export default {
   mounted: function () {
     // get yesterday's games
     var url =
-      'https://cors-anywhere.herokuapp.com/http://data.nba.net/v2015/json/mobile_teams/nba/' +
-      new Date(new Date().setFullYear(new Date().getFullYear() - 1)).toISOString().slice(0, 4) + '/scores/00_todays_scores.json'
+      'https://it.global.nba.com/stats2/scores/miniscoreboard.json' +
+    //  new Date(new Date().setFullYear(new Date().getFullYear() - 1)).toISOString().slice(0, 4) + '/scores/00_todays_scores.json'
     axios.get(url, { crossdomain: true })
       .then(response => {
         var g = []
-        for (var game of response.data.gs.g) {
-          g.push(JSON.parse('{"id":"' + game.gid + '","home":{"nickname":"' + game.h.tn + '"},"visitor":{"nickname":"' + game.v.tn + '"}}'))
+        var utc = Math.floor((new Date()).getTime() / 1000) //parseInt(round(time.time() * 1000))
+        var key = response.payload.today.games[0].profile.utcMillis < String(utc) ? 'today' : 'previous' 
+        for (var game of response.payload.[key].games) {
+          g.push(JSON.parse('{"id":"' + game.profile.gameId + '","home":{"nickname":"' + game.homeTeam.profile.abbr) + '"},"visitor":{"nickname":"' + game.awayTeam.profile.abbr + '"}}'))
         }
         response.data.sports_content = Object.assign({}, {
           games: {
